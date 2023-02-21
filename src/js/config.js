07a5eb63e0d969c9e904f1e09d1bafdb1ffb43db
milan44i <mln6stankovic@gmail.com>
2023-02-21 18:15:56 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ced81b21-fa98-41ab-ad4d-00d86c14d16a';
+export const MODAL_CLOSE_SEC = 2.5;
